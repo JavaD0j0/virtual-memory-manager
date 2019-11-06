ec2bf2c12b6676ed8c385215dabba9641f094b72
Mario Rodriguez <marior6@uci.edu>
2019-11-05 16:10:13 -0800

    starting on manager

--- src/edu/uci/ics/marior6/manager/Manager.java
+++ src/edu/uci/ics/marior6/manager/Manager.java
@@ -1,4 +1,9 @@
 package edu.uci.ics.marior6.manager;
 
 public class Manager {
+
+    public static void main(String[] args) {
+        System.out.println("Starting virtual memory manager...");
+
+    }
 }
