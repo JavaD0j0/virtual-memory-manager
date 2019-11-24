2ab09b9538f285f25238e46f9c4f33b6047090b3
Mario Rodriguez <marior6@uci.edu>
2019-11-23 16:09:09 -0800

    Basic version finished!

--- src/edu/uci/ics/marior6/manager/Manager.java
+++ src/edu/uci/ics/marior6/manager/Manager.java
@@ -25,10 +25,12 @@ public class Manager {
     private static int segment;         //s
     private static int page;            //p
     private static int word;            //w
-    private static int segment_page;    //sp
+    private static int frame;           //f
+    private static int segment_size;    //z
     private static int[] physicalMemory = new int[PHYSICAL_MEMORY_SIZE];
-    private static int[] bitMap         = new int[BITMAP_SIZE];
-    private static int[] mask           = new int[BITMAP_SIZE];
+    private static int[] bitMap         = new int[FRAME_NUMBER];
+    private static int[][] disk         = new int[FRAME_NUMBER][FRAME_SIZE];
+    private static boolean lineChange   = false;
 
     public static void main(String[] args) {
         System.out.println("Starting virtual memory manager...");
@@ -39,7 +41,6 @@ public class Manager {
         System.out.println("Current relative path: " + path);
         //initialize data structures
         manager.init();
-
         try{
             file = new File(path + "\\output.txt\\");
             if(file.exists()) {
@@ -50,17 +51,25 @@ public class Manager {
             writer = new BufferedWriter(new FileWriter(file));
             System.out.println("Output handler ready...");
             if(args.length < 2) {
-                System.out.println("Wrong number of parameters! Error...");
+                System.out.println("ERROR: Wrong number of parameters! You need the init and input file...");
                 System.exit(0);
             } else {
-                System.out.println("Input file passed as parameter to program..." + args[0]);
+                System.out.println("Input file passed as parameter to program: " + args[0] + ", " + args[1]);
                 reader1 = new BufferedReader(new FileReader(args[0]));  //Initialization file
                 reader2 = new BufferedReader(new FileReader(args[1]));  //Testing file
                 System.out.println("Input handlers ready...");
                 String line;
+                //go thru initialization file now
                 while ((line = reader1.readLine()) != null) {
                     if (!line.isEmpty()) {
-                        manager.executeArguments(line);
+                        manager.executeInitialization(line);
+                        writer.flush();
+                    }
+                }
+                //go thru input file now
+                while ((line = reader2.readLine()) != null) {
+                    if (!line.isEmpty()) {
+                        manager.executeTranslation(line);
                         writer.flush();
                     }
                 }
@@ -74,19 +83,49 @@ public class Manager {
         }
     }
 
-    private void executeArguments(String line) {
+    private void executeInitialization(String line) {
+        String[] line1 = line.split(" ");
+        System.out.println(Arrays.toString(line1));
+        //TODO: initialize ST and PT from files
+        if (!lineChange) {
+            //initialize ST
+            for (int i = 0; i <= line1.length - 3; i = i + 3) {
+                segment = Integer.parseInt(line1[i]);
+                segment_size = Integer.parseInt(line1[i + 1]);
+                frame = Integer.parseInt(line1[i + 2]);
+                System.out.println("S: " + segment + ", Z: " + segment_size + ", F: " + frame);
+                initST(segment, segment_size, frame);
+            }
+            lineChange = true;
+        } else {
+            //initialize PT
+            for (int j = 0; j <= line1.length - 3; j = j + 3) {
+                segment = Integer.parseInt(line1[j]);
+                page = Integer.parseInt(line1[j + 1]);
+                frame = Integer.parseInt(line1[j + 2]);
+                System.out.println("S: " + segment + ", P: " + page + ", F: " + frame);
+                initPT(segment, page, frame);
+            }
+            lineChange = false;
+        }
+    }
 
+    private void executeTranslation(String line) throws IOException{
+        String[] vAddresses = line.split(" ");
+        System.out.println(Arrays.toString(vAddresses));
+        //TODO: translate VA to PA
+        for (String vAddress : vAddresses) {
+            int physicalAddress = VAtoSPW(Integer.parseInt(vAddress));
+            System.out.println("PA: " + physicalAddress);
+            writer.write(physicalAddress + " ");
+        }
     }
 
     private void init() {
         System.out.println("Starting to initialize data structures...");
-        //initialize mask
-        for (int i = BITMAP_SIZE - 1; i >= 0; i--) {
-            mask[i] = 1 << (31 - i);
-        }
         //initialize bitMap (first/second frames are reserved to ST)
-        bitMap[0] = mask[0];
-        bitMap[1] = mask[1];
+        bitMap[0] = 1;
+        bitMap[1] = 1;
         for (int i = 2; i < BITMAP_SIZE; i++) {
             bitMap[i] = 0;
         }
@@ -94,47 +133,48 @@ public class Manager {
         for (int i = 0; i < PHYSICAL_MEMORY_SIZE; i++) {
             physicalMemory[i] = 0;
         }
+        //initialize disk
+        for (int i = 0; i < FRAME_NUMBER; i++) {
+            for (int j = 0; j < FRAME_SIZE; j++) {
+                disk[i][j] = 0;
+            }
+        }
         System.out.println("DONE initializing data structures!");
     }
 
-//    private void init(int segment, int frame) {
-//        physicalMemory[segment] = frame;
+    private void initST(int segment, int segment_size, int frame) {
+        physicalMemory[2 * segment] = segment_size;
+        physicalMemory[(2 * segment) + 1] = frame;
 //        if(frame != PAGE_FAULT) {
-//            //the pageTable takes two frames
+//            //ST takes two frames
 //            allocateBitmap(frame);
-//            allocateBitmap(frame + 1);
+//            allocateBitmap(frame + FRAME_SIZE);
 //        }
-//    }
-//
-//    private void init(int segment, int page, int frame) {
-//        physicalMemory[physicalMemory[segment] + page] = frame;
-//        if(frame != PAGE_FAULT) {
-//            allocateBitmap(frame);
-//        }
-//    }
+    }
 
-    private void VAtoSPW(int virtualAddress) {
-        System.out.println("Translating from VA to S,P,W...");
-        for (int i = 0; i <= 9; i++) {
-            if(i == 9 && (virtualAddress & mask[22]) != EMPTY) {
-                page = page | mask[31];
-                break;
-            }
-            //Since each entry consists of 2 integers and the ST resides in frame
-            //0 and 1, slots from 0 to 3 are taken so we start at slot 4
-            if((virtualAddress & mask[i + 4]) != EMPTY) {
-                segment = segment | mask[i + 23];
-            }
-            if((virtualAddress & mask[i + 13]) != EMPTY) {
-                page = page | mask[i + 22];
-            }
-            if((virtualAddress & mask[i + 23]) != EMPTY) {
-                word = word | mask[i + 23];
-            }
+    private void initPT(int segment, int page, int frame) {
+        physicalMemory[physicalMemory[(2 * segment) + 1] * FRAME_SIZE + page] = frame;
+//        if(frame != PAGE_FAULT) {
+//            allocateBitmap(frame);
+//        }
+    }
+
+    private int VAtoSPW(int virtualAddress) {
+        System.out.println("Translating from VA: " + virtualAddress + " to S,P,W...");
+        segment = virtualAddress >> 18;
+        word = virtualAddress & 0x1FF;
+        page = (virtualAddress >> 9) & 0x1FF;
+        int pw = virtualAddress & 0x3FFFF;
+
+        if (pw >= physicalMemory[2 * segment]) {
+            System.out.println("VA is outside the segment boundary! ERROR...");
+            return PAGE_FAULT;
+        } else {
+            return physicalMemory[physicalMemory[(2 * segment) + 1] * FRAME_SIZE + page] * FRAME_SIZE + word;
         }
     }
 
-    private int read() {
+    private int readBlock(int b, int m) {
         int p = physicalMemory[segment];
         int w = physicalMemory[physicalMemory[segment] + page];
         if (p == PAGE_FAULT || p == EMPTY) {    //check for pageFault or empty segment
@@ -145,75 +185,12 @@ public class Manager {
             System.out.println("Read: Error - 'w' page fault");
             //FIXME: return error
         }
-        //TODO: return physicalAddress (PM[PM[s] + p] + w)
+
         return w + word;
     }
 
-    private int write() {
-        int p = physicalMemory[segment];
-        int w = physicalMemory[physicalMemory[segment] + page];
-        if (p != PAGE_FAULT) {  //check for pageFault
-            if (p == EMPTY) {
-                p = allocatePageTable();
-            }
-        } else {
-            System.out.println("Write: Error - 'p' page fault");
-            //FIXME: return error
-        }
-
-        if (w != PAGE_FAULT) {  //check for pageFault
-            if (w == EMPTY) {
-                w = allocateData();
-            }
-        } else {
-            System.out.println("Write: Error - 'w' page fault");
-            //FIXME: return error
-        }
-        //TODO: return physicalAddress (PM[PM[s] + p] + w)
-        return w + word;
-    }
-
-    private void allocateBitmap(int frame) {
-        if(frame != PAGE_FAULT) {
-            for (int i = 0; i < FRAME_SIZE; i++) {
-                physicalMemory[frame + i] = 0;
-            }
-            frame = frame / FRAME_SIZE;
-            bitMap[frame / BITMAP_SIZE] = (bitMap[frame / BITMAP_SIZE] | mask[frame % BITMAP_SIZE]);
-        }
-    }
-
-    private int allocateData() {
-        int frame = EMPTY;
-        for (int i = 0; i < FRAME_NUMBER; i++) {
-            if ((bitMap[i / BITMAP_SIZE] & mask[i % BITMAP_SIZE]) == EMPTY) {
-                frame = i * FRAME_SIZE;
-                allocateBitmap(frame);
-                break;
-            }
-        }
-        return frame;
-    }
-
-    private int allocatePageTable() {
-        int frame1 = EMPTY;
-        int frame2 = EMPTY;
-        for (int i = 0; i < FRAME_NUMBER; i++) {
-            //check if bit is empty
-            if ((bitMap[i / BITMAP_SIZE] & mask[i % BITMAP_SIZE]) == EMPTY) {
-                //now check next bit if it is empty
-                if ((bitMap[(i + 1) / BITMAP_SIZE] & mask[i + 1 % BITMAP_SIZE]) == EMPTY) {
-                    frame1 = i * FRAME_SIZE;
-                    frame2 = (i + 1) * FRAME_SIZE;
-                    allocateBitmap(frame1);
-                    allocateBitmap(frame2);
-                }
-            }
-        }
-        return frame1;
-    }
-
-    private void displayBitmap() {
-        System.out.println("BitMap: " + Arrays.toString(bitMap));
+    private void display() {
+        System.out.println("PA: " + Arrays.toString(physicalMemory));
+        System.out.println("BM: " + Arrays.toString(bitMap));
     }
 }
